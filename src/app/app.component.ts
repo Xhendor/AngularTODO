34e5e9d6f1d3b9817373bc3d25a8dd8569715010
xhendor <rosendorsc@gmail.com>
2021-08-31 20:58:02 -0700

    Cambios en interfaz y submit /form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestNGAng';
+  titulo3='Prueba';
+  todo='';
+  todoArray=[];
+
+
+  addTodo( value: string ) {
+    if(value!=""){
+    // @ts-ignore
+    this.todoArray.push(value)
+    }else{
+      alert("Campo requerido***")
+    }
+    //Salida a consola
+   // console.log(this.todoArray)
+  }
+
+  deleteItem(todo: any){
+    for(let i=0;i<=this.todoArray.length;i++){
+      if(todo==this.todoArray[i]){
+        this.todoArray.splice(i,1);
+      }
+    }
+  }
+  todoSubmit(form:NgForm){
+    if(form.value!==""){
+      // @ts-ignore
+      this.todoArray.push(form.value)
+      form.resetForm("");
+      //console.log(this.todos)
+    }else{
+      alert('Field required **')
+    }
+  }
+
+
 }
